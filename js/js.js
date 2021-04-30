965ae527bc2f90f8cc5f10e9a369d728d2b04c56
BeccAYYY <becsell@live.com.au>
2021-04-30 21:49:53 +1000

    finished criteria

--- js/js.js
+++ js/js.js
@@ -1,6 +1,5 @@
 function errorDiv(input, div) {
     if (! input.checkValidity()) {
-        console.log("hi")
         document.getElementById(div).innerHTML = input.title
     } else {
         document.getElementById(div).innerHTML = ""
